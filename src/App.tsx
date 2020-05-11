f340f361458fe69e8c7f92bc6ce66991aae375f0
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-11 10:41:23 -0300

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,23 @@
 import * as React from 'react';
-import styled from 'styled-components';
-import Header from './components/Header';
 import { GlobalStyle } from './styles/global';
-import { media } from './styles/mediaQueries';
-import homeSVG from './icons/home.svg';
-import SavingCard from './components/SavingCard';
-
-const StyledPageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: #f4f8fa;
-  min-height: 100vh;
-`;
-
-const StyledTitle = styled.h1`
-  color: #1b31a8;
-  font-size: 1.8rem;
-  line-height: 1.44;
-  margin-top: 2.3rem;
-  margin-bottom: 2.4rem;
-
-  ${media.desktop} {
-    font-size: 2.4rem;
-    line-height: 1.33;
-    margin-top: 5.4rem;
-    margin-bottom: 4rem;
-  }
-
-  & > strong {
-    font-weight: 600;
-  }
-`;
-
-const StyledSavingCard = styled(SavingCard)`
-  align-self: stretch;
-
-  ${media.mobile} {
-    flex-grow: 1;
-  }
-
-  ${media.desktop} {
-    align-self: center;
-    width: 56rem;
-  }
-`;
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Form from './pages/Form';
+import Listing from './pages/Listing';
 
 const App: React.FunctionComponent = () => {
   return (
     <>
       <GlobalStyle />
-      <StyledPageContainer>
-        <Header />
-        <StyledTitle>
-          Let&apos;s plan your <strong>saving goal</strong>
-        </StyledTitle>
-        <StyledSavingCard
-          iconSrc={homeSVG}
-          iconAlt="Little house with dollar sign on the bottom left"
-          title="Buy a house"
-        />
-      </StyledPageContainer>
+      <Router>
+        <Switch>
+          <Route path="/setup">
+            <Form />
+          </Route>
+          <Route path="/">
+            <Listing />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
---
+++ src/pages/Form.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import { media } from '../styles/mediaQueries';
+import homeSVG from '../icons/home.svg';
+
+import SavingCard from '../components/SavingCard';
+import Header from '../components/Header';
+
+const StyledPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #f4f8fa;
+  min-height: 100vh;
+`;
+
+const StyledTitle = styled.h1`
+  color: #1b31a8;
+  font-size: 1.8rem;
+  line-height: 1.44;
+  margin-top: 2.3rem;
+  margin-bottom: 2.4rem;
+
+  ${media.desktop} {
+    font-size: 2.4rem;
+    line-height: 1.33;
+    margin-top: 5.4rem;
+    margin-bottom: 4rem;
+  }
+
+  & > strong {
+    font-weight: 600;
+  }
+`;
+
+const StyledSavingCard = styled(SavingCard)`
+  align-self: stretch;
+
+  ${media.mobile} {
+    flex-grow: 1;
+  }
+
+  ${media.desktop} {
+    align-self: center;
+    width: 56rem;
+  }
+`;
+
+const Form = () => (
+  <StyledPageContainer>
+    <Header />
+    <StyledTitle>
+      Let&apos;s plan your <strong>saving goal</strong>
+    </StyledTitle>
+    <StyledSavingCard
+      iconSrc={homeSVG}
+      iconAlt="Little house with dollar sign on the bottom left"
+      title="Buy a house"
+    />
+  </StyledPageContainer>
+);
+
+export default Form;
